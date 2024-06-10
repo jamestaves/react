1bf1736b09b23e1730eff00599495b52ee2ba846
jamestaves <foreveronlyme@proton.me>
2024-06-09 16:13:34 -0800

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://gastric-essy-isai-48401e66.koyeb.app/api/",
   headers: {
     "Content-type": "application/json"
   }
